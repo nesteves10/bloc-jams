3762ab7829ae19ca40aa74cad20f325499024982
Norberto Esteves <norbertoesteves@hotmail.com>
2014-09-17 22:56:23 +0000

    added a profile and subscription page with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -259,6 +259,7 @@ var setupSeekBars = function() {
 require("./landing");
 require('./collection');
 require('./album');
+require('./profile');
 
 });
 
@@ -356,6 +357,28 @@ $(document).ready(function (){
 });
 
 
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+var tabsContainer = '.user-profile-tabs-container';
+var selectTabHandler = function(event) {
+  $tab = $(this);
+  $(tabsContainer + " li").removeClass('active');
+  $tab.parent().addClass('active');
+  selectedTabName = $tab.attr('href');
+  console.log(selectedTabName);
+  $('.tab-pane').addClass('hidden');
+  $(selectedTabName).removeClass('hidden');
+  event.preventDefault();
+};
+
+if (document.URL.match(/\/profile.html/)) {
+  $(document).ready(function() {
+    var $tabs = $(tabsContainer + " a");
+    $tabs.click(selectTabHandler);
+    $tabs[0].click();
+  });
+}
 });
 
 ;
